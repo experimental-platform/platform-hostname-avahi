7e0456c868f657b6838ac87076e190018fea83a6
Kamil Domański <kamil@domanski.co>
2016-03-03 12:04:26 +0100

    TM-required services

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ set -eu
 INTERFACES=$(ip link | sed -r 's/^[0-9]*: ([a-z0-9]*).*/\1/;tx;d;:x' | grep -v '^veth' | grep -v '^docker' | grep -v '^lo$' | tr '\n' ',' | sed 's/,$//')
 BLACKLIST_INTERFACES=$(ip link | sed -r 's/^[0-9]*: ([a-z0-9]*).*/\1/;tx;d;:x' | grep '^veth' | tr '\n' ',' | sed 's/,$//')
 
-cp /etc/avahi/avahi-daemon.conf /etc/avahi/avahi-daemon.conf.bak
-
 sed "s|#allow-interfaces=.*|allow-interfaces=$INTERFACES|" -i /etc/avahi/avahi-daemon.conf
 #sed "s|deny-interfaces=.*|deny-interfaces=$BLACKLIST_INTERFACES|" -i /etc/avahi/avahi-daemon.conf
 
